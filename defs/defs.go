5004958774cbfd8598498e3feaa7f5d3b070b0a2
Paul F. Dunn <paulfdunn@gmail.com>
2023-01-22 10:18:11 -0700

    Added a few more default symbols

--- defs/defs.go
+++ defs/defs.go
@@ -4,5 +4,14 @@ const (
 	AppName         = "go-quantstudio"
 	MALengthDefault = 200
 	MASplitDefault  = 0.05
-	SymbolsDefault  = "dia,spy,qqq,ddm,qld,sso"
+	// DIA	SPDR Dow Jones Industrial Average ETF Trust
+	// IWB	iShares Russell 1000 ETF
+	// QQQ	Invesco QQQ Trust Series 1
+	// RSP	Invesco S&P 500 Eql Wght ETF
+	// SPY	SPDR S&P 500 ETF Trust
+	// Double ETFs below
+	// DDM	ProShares Ultra Dow30
+	// QLD	ProShares Ultra QQQ
+	// SSO	ProShares Ultra S&P500
+	SymbolsDefault = "dia,iwb,rsp,spy,qqq,ddm,qld,sso"
 )
