b2a2bba70cb24ee3d43aa02bf60b4e089945c2d3
Paul F. Dunn <paulfdunn@gmail.com>
2023-05-01 16:00:11 -0600

    remove the run.sh script

--- run.sh
+++
@@ -1,9 +0,0 @@
-# Use this script to run both go-quantstudio and automator, or 
-# open a terminal and run:
-# go build && ./go-quantstudio --logfile="" 
-# open a second terminal and run:
-# go run automator.go --logfile=""
-go build && ./go-quantstudio &
-sleep 10
-cd automator
-go build && ./automator
